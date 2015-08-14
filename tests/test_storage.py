03d76e404bdcfaaa067b1532da26026c9af509d1
Matthew Norman <mnorman.hep@gmail.com>
2015-08-13 20:44:56 -0700

    Tests for storage updated

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -10,3 +10,10 @@ def test_integration_storage():
     engine = storage.StorageEngine(config=config)
     result = engine.cursor.execute("SELECT COUNT(*) FROM test")
     assert result == 1
+
+    try:
+        engine._create_tables()
+        res = engine.cursor.execute("SELECT * FROM components")
+        assert res == 0
+    finally:
+        engine._drop_tables()
