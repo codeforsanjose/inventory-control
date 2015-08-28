386ccacd9e859b0c532f646ade366cf0189a6699
Matthew Norman <mnorman.hep@gmail.com>
2015-08-27 20:17:48 -0700

    Component type accessors

--- inventory_control/storage.py
+++ inventory_control/storage.py
@@ -40,6 +40,40 @@ class StorageEngine(object):
         :return:
         """
 
+    def add_component_type(self, type_name):
+        """
+        Add a component type that DOESN'T exist in the DB
+        :param type_name: Text string of the type.
+        :return:
+        """
+        str = sql.ADD_COMPONENT_TYPE.format(text=type_name)
+        self.cursor.execute(str)
+
+    def remove_component_type(self, type_name):
+        """
+        Given a type name, delete a component type
+        that matches it.
+
+        :param type_name: Text field in the DB.
+        :return:
+        """
+        self.cursor.execute(sql.DELETE_COMPONENT_TYPE.format(text=type_name))
+
+    def get_component_type(self, type_name):
+        """
+        Get a component type based on the type_name
+
+        Returns None if component_type does not exist.
+
+        :param type_name:
+        :return:
+        """
+        self.cursor.execute(sql.GET_COMPONENT_TYPE.format(text=type_name))
+        component_type =  self.cursor.fetchone()
+        if component_type is None:
+            return None
+        return {'ID': component_type[0], 'type': component_type[1]}
+
     def _drop_tables(self):
         """
         Dump all the tables
